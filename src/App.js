32ad5cad163454eabb8cdcf6af50c70f3bc6b696
Estifanos Kidane <ekidane12@gmail.com>
2023-01-04 15:18:37 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import HomePage from './components/HomePage'
+import EmployeePage from "./components/EmployeePage";
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello from React</h2>
+      <div style={style}>
+        <HomePage />
+        <EmployeePage />
+      </div>
     </div>
   );
 }
+
+const style = {
+  display: "flex",
+  border: "black 1.5px solid",
+  margin: "5px",
+  
+};
 
 export default App;
